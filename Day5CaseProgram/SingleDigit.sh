57ce36e3c9c117120d2a078a5307f548fbc3a585
Roshni1998 <roshnimali847@gmail.com>
2021-05-07 17:04:13 +0530

    ADDED: DISPLAY_A_SINGLE_DIGIT_IN_WORD

--- Day5CaseProgram/SingleDigit.sh
+++ Day5CaseProgram/SingleDigit.sh
@@ -1,38 +1,28 @@
 #!/bin/bash -x
-N0=0 N1=1 N2=2 N3=3 N4=4 N5=5 N6=6 N7=7 N8=8 N9=9
-read -p "Enter a Single Digit Number : " N
 
-case $N in
-   $N0)
-      echo "Your Number $N in words: Zero"
-      ;;
-   $N1)
-      echo "Your Number $N in words: One"
-      ;;
-   $N2)
-      echo "Your Number $N in words: Two"
-      ;;
-   $N3)
-      echo "Your Number $N in words: Three"
-      ;;
-   $N4)
-      echo "Your Number $N in words: Four"
-      ;;
-   $N5)
-      echo "Your Number $N in words: Five"
-      ;;
-   $N6)
-      echo "Your Number $N in words: Six"
-      ;;
-   $N7)
-      echo "Your Number $N in words: Seven"
-      ;;
-   $N8)
-      echo "Your Number $N in words: Eight"
-      ;;
-   $N9)
-      echo "Your Number $N in words: Nine"
-      ;;
+read -p "Enter a Single Digit Number : " num
+case $num in
+   0)
+   echo "Your Number $N in words: Zero";;
+   1)
+   echo "Your Number $N in words: One";;
+   2)
+   echo "Your Number $N in words: Two";;
+   3)
+   echo "Your Number $N in words: Three";;
+   4)
+   echo "Your Number $N in words: Four";;
+   5)
+   echo "Your Number $N in words: Five";;
+   6)
+   echo "Your Number $N in words: Six";;
+   7)
+   echo "Your Number $N in words: Seven";;
+   8)
+   echo "Your Number $N in words: Eight";;
+   9)
+   echo "Your Number $N in words: Nine";;
    *)
+   echo "Please Enter a Number between 0 to 9";;
 esac
 
