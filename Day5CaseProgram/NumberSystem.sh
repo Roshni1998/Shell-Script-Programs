35870fa9db66ce865adb98e5b0374f83fc261da2
Roshni1998 <roshnimali847@gmail.com>
2021-05-07 17:06:00 +0530

    ADDED: DISPLAY_A_PLACE_VALUE

--- Day5CaseProgram/NumberSystem.sh
+++ Day5CaseProgram/NumberSystem.sh
@@ -1,46 +1,26 @@
 #!/bin/bash -x
-N1=1
-N2=10
-N3=100
-N4=1000
-N5=10000
-N6=100000
-N7=1000000
-N8=10000000
-N9=100000000
-
+n
 read -p "Enter a Place Value Number : " PlaceValue
-
 case $PlaceValue in
-   $N1)
-      echo "Place Value is: UNIT"
-      ;;
-   $N2)
-      echo "Place Value is: TEN"
-      ;;
-   $N3)
-      echo "Place Value is: HUNDRED"
-      ;;
-   $N4)
-      echo "Place Value is: THOUSAND"
-      ;;
-   $N5)
-      echo "Place Value is: TEN THOUSAND"
-      ;;
-   $N6)
-      echo "Place Value is: LAKH"
-      ;;
-   $N7)
-      echo "Place Value is: TEN LAKH"
-      ;;
-   $N8)
-      echo "Place Value is: CRORE"
-      ;;
-   $N9)
-      echo "Place Value is: TEN CRORE"
-      ;;
-   *)
-   exit
+   1)
+      echo "Place Value is: UNIT";;
+   10)
+      echo "Place Value is: TEN";;
+   100)
+      echo "Place Value is: HUNDRED";;
+   1000)
+      echo "Place Value is: THOUSAND";;
+   10000)
+      echo "Place Value is: TEN THOUSAND";;
+   100000)
+      echo "Place Value is: LAKH";;
+   1000000)
+      echo "Place Value is: TEN LAKH";;
+   10000000)
+      echo "Place Value is: CRORE";;
+   100000000)
+      echo "Place Value is: TEN CRORE";;
+   *) echo "Please Enter a Number in a format of 10^n";;
 esac
 
 
