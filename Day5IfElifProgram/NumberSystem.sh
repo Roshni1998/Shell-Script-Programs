666546cea8ed1df15c7981c4451749ff673ec7dd
Roshni1998 <roshnimali847@gmail.com>
2021-05-07 20:27:43 +0530

    ADDED: DISPLAY_PLACE_VALUE

--- Day5IfElifProgram/NumberSystem.sh
+++ Day5IfElifProgram/NumberSystem.sh
@@ -3,43 +3,34 @@ read -p "Enter a Place Value Number : " N
 
 if [ $N -eq 1 ]
 then
-	echo "Place Value is: UNIT"
+   echo "Place Value is: UNIT"
 elif [ $N -eq 10 ]
 then
-	echo "Place Value is: TEN"
+   echo "Place Value is: TEN"
+elif [ $N -eq 100 ]
+then
+   echo "Place Value is: HUNDRED"
+elif [ $N -eq 1000 ]
+then
+   echo "Place Value is: THOUSAND"
+elif [ $N -eq 10000 ]
+then
+   echo "Place Value is: TEN THOUSAND"
+elif [ $N -eq 100000 ]
+then
+   echo "Place Value is: LAKH"
+elif [ $N -eq 1000000 ]
+then
+   echo "Place Value is: TEN LAKH"
+elif [ $N -eq 10000000 ]
+then
+   echo "Place Value is: Crore"
+elif [ $N -eq 100000000 ]
+then
+   echo "Place Value is: TEN CRORE"
 else
-		if [ $N -eq 100 ]
-		then
-   		echo "Place Value is: HUNDRED"
-		elif [ $N -eq 1000 ]
-		then
-   		echo "Place Value is: THOUSAND"
-		else
-				if [ $N -eq 10000 ]
-				then
-   				echo "Place Value is: TEN THOUSAND"
-				elif [ $N -eq 100000 ]
-				then
-   				echo "Place Value is: LAKH"
-				else
-						if [ $N -eq 1000000 ]
-						then
-   						echo "Place Value is: TEN LAKH"
-						elif [ $N -eq 10000000 ]
-						then
-   						echo "Place Value is: Crore"
-						else
-							if [ $N -eq 100000000 ]
-							then
-   							echo "Place Value is: TEN CRORE"
-							else
-								N=0
-							fi
-						fi
-				fi
-		fi
-fi					
-		
+   echo "Please enter a value in the format of 10^8"
+fi
 
 
 
