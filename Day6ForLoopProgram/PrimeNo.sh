0317492668bd780e62ba5a6cc5ee6892fd81d8a1
Roshni1998 <roshnimali847@gmail.com>
2021-05-07 19:33:10 +0530

    ADDED: TO_CHECK_NUMBER_IS_PRIME_OR_NOT

--- Day6ForLoopProgram/PrimeNo.sh
+++ Day6ForLoopProgram/PrimeNo.sh
@@ -1,13 +1,15 @@
+#!/bin/bash -x
+
 #To check entered number is Prime or not
 echo "Enter Number"
 read Num
 for((i=2;i<=$Num/2;i++))
 do
-	if [ $((Num%i)) -eq 0 ]
-	then
-		echo "$Num is not a Prime Number."
-	exit
-	fi
+   if [ $((Num%i)) -eq 0 ]
+   then
+      echo "$Num is not a Prime Number."
+      exit
+   fi
 done
 echo "$Num is a Prime Number."
 
